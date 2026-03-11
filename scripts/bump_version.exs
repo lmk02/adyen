feefe7ca20882b26b26cb409a36ef2c35bd8a564
Leon Koth <leokoth@stagedates.com>
2026-03-11 16:56:14 +0100

    chore: debug

--- scripts/bump_version.exs
+++ scripts/bump_version.exs
@@ -7,29 +7,35 @@ defmodule BumpVersion do
     # Regex to find version: "x.y.z"
     regex = ~r/version:\s*"(\d+)\.(\d+)\.(\d+)"/
     
-    new_version = 
-      case args do
-        [v] -> v
-        _ -> 
-          case Regex.run(regex, content) do
-            [_full_match, major, minor, patch] ->
-              new_patch = String.to_integer(patch) + 1
-              "#{major}.#{minor}.#{new_patch}"
-            _ ->
-              IO.puts(:stderr, "Could not find version in mix.exs and no version provided")
-              System.halt(1)
-          end
+    {current_version, major, minor, patch} = 
+      case Regex.run(regex, content) do
+        [full_match, major, minor, patch] ->
+          {String.replace(full_match, ~r/version:\s*"/, "") |> String.replace("\"", ""), major, minor, patch}
+        _ ->
+          IO.puts(:stderr, "Could not find version in mix.exs")
+          System.halt(1)
       end
 
-    case Regex.run(regex, content) do
-      [full_match | _] ->
-        new_content = String.replace(content, full_match, "version: \"#{new_version}\"")
-        File.write!(path, new_content)
-        IO.puts(new_version) # Output the new version for the shell script
+    new_version = 
+      case args do
+        ["--current"] ->
+          IO.puts(current_version)
+          System.halt(0)
 
-      _ ->
-        IO.puts(:stderr, "Could not find version in mix.exs")
-        System.halt(1)
+        [v] when is_binary(v) -> v
+
+        _ -> 
+          new_patch = String.to_integer(patch) + 1
+          "#{major}.#{minor}.#{new_patch}"
+      end
+
+    if new_version != current_version do
+      new_content = String.replace(content, "version: \"#{current_version}\"", "version: \"#{new_version}\"")
+      File.write!(path, new_content)
+      IO.puts(new_version)
+    else
+      IO.puts(:stderr, "Version is already #{new_version}, no change made.")
+      System.halt(0) # Exit with 0 but no output to stdout, or handle in shell
     end
   end
 end
